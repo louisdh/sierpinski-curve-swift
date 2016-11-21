08031ad3521b7fa9bae2ad8dd08f8c1b65a1403a
Louis D'hauwe <louisdhauwe@silverfox.be>
2016-11-21 21:20:48 +0100

    Initial Commit

---
+++ Sierpinski/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sierpinski
+//
+//  Created by Louis D'hauwe on 21/11/2016.
+//  Copyright © 2016 Silver Fox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
